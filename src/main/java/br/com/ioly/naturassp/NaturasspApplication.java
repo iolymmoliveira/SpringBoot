c162795bf3d269e35b29c6b029d3466d3c6be437
iolyOliveira <iolymmoliveira@gmail.com>
2023-08-29 15:20:00 -0300

    Configuracao do Projeto

---
+++ src/main/java/br/com/ioly/naturassp/NaturasspApplication.java
@@ -0,0 +1,14 @@
+package br.com.ioly.naturassp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturasspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturasspApplication.class, args);
+		
+	}
+
+}
